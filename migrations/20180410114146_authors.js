5c5e37e0fc8e42dc5e74eeb5fe1e8083e04c9339
Chris Casey <cdcasey@gmail.com>
2018-04-10 15:15:41 -0500

    Formatting

--- migrations/20180410114146_authors.js
+++ migrations/20180410114146_authors.js
@@ -1,18 +1,18 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return createAuthors()
-  .then(createBooks)
+    .then(createBooks)
 
-  function createAuthors(){
-    return knex.schema.createTable('authors', function(table){
+  function createAuthors() {
+    return knex.schema.createTable('authors', function (table) {
       table.increments();
       table.string('name');
       table.integer('age');
     })
   }
 
-  function createBooks(){
-    return knex.schema.createTable('books', function(table){
+  function createBooks() {
+    return knex.schema.createTable('books', function (table) {
       table.increments();
       table.string('title');
       table.integer('author_id');
@@ -21,6 +21,6 @@ exports.up = function(knex, Promise) {
   }
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return Promise.all([knex.schema.dropTable("books"), knex.schema.dropTable("authors")]);
 };

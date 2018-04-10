f9c0efff95363a057e901de2b96bbd2c76576768
Chris Casey <cdcasey@gmail.com>
2018-04-10 15:25:02 -0500

    Fixed bug that threw off auto id incrementing.

--- seeds/authors.js
+++ seeds/authors.js
@@ -1,8 +1,8 @@
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   return Promise.all([
     knex('authors').del(),
-    knex('authors').insert({id: 1, name: 'bob', age: 22}),
-    knex('authors').insert({id: 2, name: 'tom', age: 32}),
-    knex('authors').insert({id: 3, name: 'jane', age: 42}),
+    knex('authors').insert({ name: 'bob', age: 22 }),
+    knex('authors').insert({ name: 'tom', age: 32 }),
+    knex('authors').insert({ name: 'jane', age: 42 }),
   ]);
 };
